d3040433c5d02a8cc484b86e248a1a03b8e09940
Matt Burton <matt@plain.black>
2022-03-31 10:37:41 +0000

    Updated Title

--- nuxt.config.js
+++ nuxt.config.js
@@ -9,8 +9,8 @@ export default {
 
   // Global page headers: https://go.nuxtjs.dev/config-head
   head: {
-    titleTemplate: '%s - nuxt-firebase-auth',
-    title: 'nuxt-firebase-auth',
+    titleTemplate: 'dsp3000.games',
+    title: 'dsp3000.games',
     meta: [
       { charset: 'utf-8' },
       { name: 'viewport', content: 'width=device-width, initial-scale=1' },
