4c19e7ebe6db2f0885e256c9a6437a01281ee92f
Matt Burton <matt@plain.black>
2022-04-22 11:00:11 +0000

    Updated auth routes

--- middleware/auth.js
+++ middleware/auth.js
@@ -1,5 +1,5 @@
 export default function ({app, route, redirect}){
-  if (route.path !== '/auth/signin' && route.path !== '/' && route.path !== '/introduction' && route.path !== '/characters' && route.path !== '/episodes' && route.path !== '/about') {
+  if (route.path !== '/auth/signin' && route.path !== '/' && route.path !== '/introduction' && route.path !== '/characters' && route.path !== '/episodes' && route.path !== '/about'  && route.path !== '/introductionDetails'   && route.path !== '/episode1Details' && route.path !== '/episode2Details') {
     //we are on a protected route
     if(!app.$fire.auth.currentUser) {
       //take them to sign in page
