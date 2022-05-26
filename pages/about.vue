b12b246e779c852cdb27c701b21bdfd2f6ed9952
dsp3000-games <106341358+dsp3000-games@users.noreply.github.com>
2022-05-26 19:52:28 +0100

    Corrected errors on About page

--- pages/about.vue
+++ pages/about.vue
@@ -18,7 +18,7 @@ A new <b>Introductory Episode</b> available April 2022 : 180 pages<br/>
 <br/>
 <b>Chapters One and Two </b> were originally released in April 2016 as 'Academy: Part One' : 1230 pages<br/>
 This part of the story takes place during Monday morning and afternoon.<br/>
-The remastered and expanded versions available May 2022: 1600 pages <br/>
+The remastered and expanded versions available May 2022: 1600 pages<br/>
 <br/>
 <b>Chapters Three and Four</b> were originally released in July 2016 as 'Academy: Part Two' : 1851 pages<br/>
 This part of the story takes place during Tuesday morning, afternoon and early evening.<br/>
@@ -32,15 +32,15 @@ New release date TBC<br/>
 <b>Chapters Seven and Eight</b> were originally released in May 2017 as 'Academy: Part Four' : 2226 pages<br/>
 This part of the story takes place during Thursday morning and afternoon.<br/>
 The remastered and expanded versions are in early remake/production stages :<br/>
-New release date TBC
-<br/><br/>
+New release date TBC<br/>
+<br/>
 <b>Chapter Nine</b> was originally released in December 2017 as 'A date with Bridgette : Part One' : 967 pages<br/>
-This part of the story takes place during Thursday early evening.<br/>
+This part of the story takes place during early evening on Thursday.<br/>
 The remastered and expanded version is in early remake/production stage :<br/>
 New release date TBC<br/>
 <br/>
 <b>Chapter Ten</b> was originally released in January 2018 as 'A date with Bridgette : Part Two' : 1666 pages<br/>
-This part of the story takes place during Thursday later evening.<br/>
+This part of the story takes place later on during Thursday evening.<br/>
 The remastered and expanded version is in early remake/production stage :<br/>
 New release date TBC<br/>
 <br/>
@@ -54,14 +54,14 @@ This part of the story takes place late on Thursday night/Friday morning.<br/>
 The remastered and expanded version is in early remake/production stage :<br/>
 New release date TBC<br/>
 <br/>
-<b>Chapter Thirteen</b> Completed mid 2020 - yet to be released : 3002 pages
+<b>Chapter Thirteen</b> Completed mid 2020 - yet to be released : 3002 pages<br/>
 This part of the story will take place during Thursday evening.<br/>
 This brand new chapter is complete subject to testing:<br/>
 Release date TBC<br/>
 <br/>
 <b>Chapter Fourteen</b> Completed mid 2021 - yet to be released : 2900 pages<br/>
 This part of the story will take place during Friday morning.<br/>
-This brand new chapter is in complete subject to testing :<br/>
+This brand new chapter is complete subject to testing :<br/>
 Release date TBC<br/>
 <br/>
 <b>Chapter Fifteen</b> In early stages of production :<br/>
