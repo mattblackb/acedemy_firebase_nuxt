370430a40b265344a39516dcbf6158fdce09ff9a
Matt Burton <matt@plain.black>
2022-06-15 15:37:56 +0000

    Updated chapter page and profile link

--- pages/chapter3saved.vue
+++ pages/chapter3saved.vue
@@ -8,7 +8,7 @@
        <h1>Chapter 3</h1>
         <div class="container">
            <iframe
-               src="../chapter2/game/checksave2.html"
+               src="../chapter3/game/checksave3.html"
             width="100%"
             height= auto
             style="border: 1px solid #EEE; background: white"
