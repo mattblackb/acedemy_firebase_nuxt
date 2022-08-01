76cca1f985de5f8e0938c0e849fc9b8fbb93c652
dsp3000-games <106341358+dsp3000-games@users.noreply.github.com>
2022-08-01 14:00:23 +0100

    Added carousel image

--- pages/index.vue
+++ pages/index.vue
@@ -75,7 +75,7 @@ export default {
             currentUser: false,
             selectedImage: 'imgs/4.png',
             images: [
-                'imgs/4.png', 'imgs/1.png', 'imgs/2.png', 'imgs/3.png', 'imgs/5.png', 'imgs/6.png', 'imgs/7.png', 'imgs/8.png', 'imgs/9.png', 'imgs/10.png', 'imgs/11.png', 'imgs/12.png'
+                'imgs/4.png', 'imgs/1.png', 'imgs/2.png', 'imgs/3.png', 'imgs/5.png', 'imgs/6.png', 'imgs/7.png', 'imgs/8.png', 'imgs/9.png', 'imgs/10.png', 'imgs/11.png', 'imgs/12.png', 'imgs/13.png'
             ]
 		}
 	},
