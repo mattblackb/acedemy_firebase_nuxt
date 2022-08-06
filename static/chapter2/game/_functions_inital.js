324d5a1a37e6e8825d5b5d0e52b6120fb0066bb8
Matt Burton <matt@plain.black>
2022-08-06 14:47:14 +0000

    Updated functions

--- static/chapter2/game/_functions_inital.js
+++ static/chapter2/game/_functions_inital.js
@@ -54,9 +54,9 @@ function setCookiesOnEntry() {
 
 	}
 } 
-if(readVar('ch2_complete') === 0) {
+// if(readVar('ch2_complete') === 0) {
     setCookiesOnEntry();
-}
+// }
 
 if(parent.c_1){
 } else {
