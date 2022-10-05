e717db85a4038fbbfab8ea5d5bdd4c118b2e0e37
dsp3000-games <106341358+dsp3000-games@users.noreply.github.com>
2022-10-05 18:03:37 +0100

    Corrected links

--- pages/chapter2bonus1test.vue
+++ pages/chapter2bonus1test.vue
@@ -8,7 +8,7 @@
        <h1></h1>
         <div class="container">
            <iframe
-                src="../chapter1/game/mediajodie60_locked.html"
+                src="../chapter2/game/mediajodie60_locked.html"
             width="100%"
             height= auto
             style="border: 1px solid #EEE; background: white"
--- pages/chapter4bonus3test.vue
+++ pages/chapter4bonus3test.vue
@@ -8,7 +8,7 @@
        <h1></h1>
         <div class="container">
            <iframe
-                src="../chapter1/game/holly106_locked.html"
+                src="../chapter4/game/holly106_locked.html"
             width="100%"
             height= auto
             style="border: 1px solid #EEE; background: white"
