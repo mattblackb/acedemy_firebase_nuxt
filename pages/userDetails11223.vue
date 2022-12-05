0b3b740617b034aec7bb58a1abadf92c6bf702e7
Matthew Burton <matthewburton@Matthews-Mac-Studio.local>
2022-12-05 09:51:38 +0000

    Updated user lookup - cur removed data - manipulation

--- pages/userDetails11223.vue
+++ pages/userDetails11223.vue
@@ -1,60 +1,70 @@
 <template>
-<div id="app">
-  <div class="search-wrapper">
-         <label>Search email:</label>
-    <input type="text" v-model="search" placeholder="Search email.."/>
-   
-  </div>
-  <div class="wrapper">
-    <div v-for="item in returnCurrent(this.$store.state.stories.alPeople)" :key="item.id">
-      {{item.id}} | {{item.email}}
+  <div id="app">
+    <div class="search-wrapper">
+      <label>Search email:</label>
+      <input type="text" v-model="search" placeholder="Search email.." />
+      <button @click="returnCurrent">Search</button>
     </div>
-   
-
+    <div class="wrapper" v-if="this.chosenUser.length != 0">
+      {{ this.chosenUser.id }} || {{ this.chosenUser.data.email }} ||
+      {{ this.chosenUser.data.credits }}
+    </div>
+    <div>
+      <input
+        type="text"
+        v-model="creditsvalue"
+        placeholder="Add credit value..."
+      />
+      <v-btn color="success" class="mr-4" @click="addCredits">
+        Add Credits
+      </v-btn>
+    </div>
   </div>
-</div>
 </template>
 
-
 <script>
-import { mapActions } from "vuex";
-export default {  
-    data () {
-      return {
-        dialog: false,
-        dialogSave: false,
-        cookieJson: '',
-        currentCreditsneeded: [],
-         currentmodule: "",
-         search: ""
-      }
-    },
-     methods: {   ...mapActions(["stories/setAllPeople"]),
-      returnCurrent(alPeople) {
-        const that = this;
-        var returnedArray = [];
-        const map1 = alPeople.map(function(x){
-           if(that.search === '') {
-          returnedArray.push({ id: x.id, email: x.email })
-           } else{
-             let result = x.email.includes(that.search);
-             if(result){
-                returnedArray.push({ id: x.id, email: x.email })
-             }
-           }
-      });
-        return returnedArray
-      }
-      },
-    mounted() {
-       this.$store.dispatch('stories/setAllPeople')
-    },
-    computed:{
-    
-      userDetails (){
-       console.log(this.state)
-      }
-      
+import { mapActions, mapGetters } from 'vuex'
+import _ from 'lodash'
+export default {
+  ...mapGetters('setuser', ['getchosenPerson']),
+  data() {
+    return {
+      dialog: false,
+      dialogSave: false,
+      cookieJson: '',
+      currentCreditsneeded: [],
+      currentmodule: '',
+      search: '',
+      currentUser: {},
+      creditsvalue: 0,
     }
+  },
+  methods: {
+    ...mapActions(['setuser/setPerson']),
+
+    returnCurrent() {
+      const that = this
+      var returnedArray = []
+      this.$store.dispatch('setuser/setPerson', this.search)
+
+      return returnedArray
+    },
+    addCredits() {
+      let personData = { ...this.$store.state.setuser.chosenPerson }
+      let clonededPerson = _.cloneDeep(personData)
+      clonededPerson.data.credits =
+        parseInt(clonededPerson.data.credits) + parseInt(this.creditsvalue)
+      console.log(clonededPerson.data)
+      this.$store.commit('setuser/updatePerson', clonededPerson.data)
+      this.$store.commit('SET_PEOPLE', personData)
+    },
+  },
+  mounted() {},
+  computed: {
+    chosenUser() {
+      console.log(this.$store.state.setuser.chosenPerson)
+      return this.$store.state.setuser.chosenPerson
+    },
+  },
 }
 </script>
--- store/setuser.js
+++ store/setuser.js
@@ -1,26 +1,65 @@
 export const state = () => ({
-    person: []
-  })
-  
-  export const mutations = {
-    async updatePerson(state, docData) {
-        console.log('UpdatePerson', docData);
-        var userProfile = await  this.$fire.firestore.collection('People').where("email", "==", docData.email).get().then(querySnapshot => {
-          // console.log('userProfile', querySnapshot.docs[0]);
-          // console.log('ID', querySnapshot.docs[0].id);
-          this.$fire.firestore.collection('People').doc(querySnapshot.docs[0].id).set(docData);
-        })
-        //set person data state
+  person: [],
+  chosenPerson: [],
+})
 
-        //state.commit("SET_USER", userProfile);
+export const actions = {
+  async setPerson(state, email) {
+    await this.$fire.firestore
+      .collection('People')
+      .where('email', '==', email)
+      .get()
+      .then((querySnapshot) => {
+        // console.log('userProfile', querySnapshot.docs[0]);
+        // console.log('ID', querySnapshot.docs[0].id);
+        if (querySnapshot.docs[0]) {
+          var returnUser = {}
+          returnUser.id = querySnapshot.docs[0].id
+          returnUser.data = querySnapshot.docs[0].data()
+          state.commit('SET_CHOSENPERSON', returnUser)
+        } else {
+          console.log('no user')
+          state.commit('SET_CHOSENPERSON', [])
+        }
+      })
+  },
+}
+export const mutations = {
+  async updatePerson(state, docData) {
+    console.log('UpdatePerson', docData)
+    var userProfile = await this.$fire.firestore
+      .collection('People')
+      .where('email', '==', docData.email)
+      .get()
+      .then((querySnapshot) => {
+        // console.log('userProfile', querySnapshot.docs[0]);
+        // console.log('ID', querySnapshot.docs[0].id);
+        this.$fire.firestore
+          .collection('People')
+          .doc(querySnapshot.docs[0].id)
+          .set(docData)
+      })
+    //set person data state
 
-       
-        // if(userProfile.docs[0].id){
-        //   console.log('ID', docs[0].id);
-        // this.$fire.firestore.collection('People').doc(userProfile.docs[0].id).set(docData);
-        // }
-      },
-    clearPeople({ commit }) {
-        commit("SET_PEOPLE", []);
-      }
-  }
+    //state.commit("SET_USER", userProfile);
+
+    // if(userProfile.docs[0].id){
+    //   console.log('ID', docs[0].id);
+    // this.$fire.firestore.collection('People').doc(userProfile.docs[0].id).set(docData);
+    // }
+  },
+
+  SET_CHOSENPERSON(state, chosenPerson) {
+    state.chosenPerson = chosenPerson
+  },
+
+  clearPeople({ commit }) {
+    commit('SET_PEOPLE', [])
+  },
+}
+
+export const getters = {
+  getchosenPerson(state) {
+    return state.chosenPerson
+  },
+}
