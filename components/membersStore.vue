a9274e17ddfdb78285b24a5faf5c161d3274f7e8
Matthew Burton <matthewburton@Matthews-Mac-Studio.local>
2022-12-10 12:56:52 +0000

    Update test

---
+++ components/membersStore.vue
@@ -0,0 +1,31 @@
+<template>
+  <div>
+    {{ currentItem }} {{ indexProps }} {{ returnCorrectArray(currentItem) }}
+  </div>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      stored: [],
+    }
+  },
+  props: {
+    currentItem: [],
+    indexProps: 0,
+  },
+  methods: {
+    returnCorrectArray(item) {
+      //loop arround this.currentItem
+
+      var arr = ''
+
+      Object.values(item).forEach((value) => {
+        arr = arr + value
+      })
+      console.log('arr', arr)
+    },
+  },
+}
+</script>
--- pages/userDetails11223.vue
+++ pages/userDetails11223.vue
@@ -10,6 +10,18 @@
       {{ this.chosenUser.data.credits }}
     </div>
     <div>
+      <div class="" v-if="this.chosenUser.length != 0">
+        <div
+          v-for="(item, index) in this.chosenUser.data.saved_games"
+          :key="index"
+        >
+          {{ item[0] }}
+          <div v-if="item[0] === '{'">
+            {{ setUnwanted() }}
+          </div>
+        </div>
+      </div>
+      <hr />
       <input
         type="text"
         v-model="creditsvalue"
@@ -17,6 +29,15 @@
       />
       <v-btn color="success" class="mr-4" @click="addCredits">
         Add Credits
+      </v-btn>
+    </div>
+    <div class="unWantedWarning" v-if="unwantedRubbish">
+      <h1>Unwanted Rubbish</h1>
+      <h3>
+        This user has unwanted items in his/her saved games - Remove them?
+      </h3>
+      <v-btn color="success" class="mr-4" @click="removeUnwantedRubbish">
+        Remove
       </v-btn>
     </div>
   </div>
@@ -37,6 +58,7 @@ export default {
       search: '',
       currentUser: {},
       creditsvalue: 0,
+      unwantedRubbish: false,
     }
   },
   methods: {
@@ -54,9 +76,35 @@ export default {
       let clonededPerson = _.cloneDeep(personData)
       clonededPerson.data.credits =
         parseInt(clonededPerson.data.credits) + parseInt(this.creditsvalue)
-      console.log(clonededPerson.data)
       this.$store.commit('setuser/updatePerson', clonededPerson.data)
       this.$store.commit('SET_PEOPLE', personData)
+    },
+    setUnwanted() {
+      this.unwantedRubbish = true
+    },
+    removeUnwantedRubbish() {
+      let personData = { ...this.$store.state.setuser.chosenPerson }
+      let clonededPersonr = _.cloneDeep(personData)
+      this.chosenUser.data.saved_games.forEach((value, index) => {
+        console.log('index', index)
+        if (value[0] === '{') {
+          //lo0p around data and remove unwanted items
+          console.log(this.chosenUser.data.saved_games[index])
+          for (const [key, value] of Object.entries(
+            this.chosenUser.data.saved_games[index]
+          )) {
+            if (!isNaN(key)) {
+              delete clonededPersonr.data.saved_games[index][key]
+            }
+          }
+          // this.chosenUser.data.saved_games[index].splice(index, 1)
+        }
+      })
+      console.log('clonededPerson2', clonededPersonr)
+      this.$store.commit('setuser/updatePerson', clonededPersonr.data)
+
+      this.unwantedRubbish = true
+      // this.$store.commit('SET_PEOPLE', personData)
     },
   },
   mounted() {},
@@ -68,3 +116,9 @@ export default {
   },
 }
 </script>
+<style scoped>
+.unWantedWarning {
+  background: white !important;
+  border: 2px solid red !important;
+}
+</style>
