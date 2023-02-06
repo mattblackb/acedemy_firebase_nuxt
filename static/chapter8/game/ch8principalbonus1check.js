b22d983601af0fbe7a07ffcb1934b4b4aa8020fa
dsp3000-games <106341358+dsp3000-games@users.noreply.github.com>
2023-02-06 15:17:32 +0000

    Ch8 mods

---
+++ static/chapter8/game/ch8principalbonus1check.js
@@ -0,0 +1,21 @@
+// displays ch8principal_ outcome
+
+function outcome()
+
+{
+
+  var val = readVar("ch8principal_");
+
+	
+
+    if (val > 0)
+
+        document.write("<img src='buttonsetc/endcards/ch8principalbonus1.jpg'>"); 
+
+    else
+	
+	    document.write("<img src='buttonsetc/endcards/ach_blank.jpg'>");
+
+}
+
+outcome();
