81999243f6855a95bf40c752e83811264fe7691d
dsp3000-games <106341358+dsp3000-games@users.noreply.github.com>
2023-06-15 17:08:18 +0100

    Amendments and missing checkfile

---
+++ static/chapter10/game/ch10bonus2check.js
@@ -0,0 +1,21 @@
+// displays bonus outcome
+
+function outcome()
+
+{
+
+  var val = readVar("ch10lolaviewbonus");
+
+	
+
+    if (val > 0)
+
+        document.write("<img src='buttonsetc/endcards/ch10bonus2.jpg'>"); 
+
+    else
+	
+	    document.write("<img src='buttonsetc/endcards/ch10bonus2_blank.jpg'>");
+
+}
+
+outcome();
