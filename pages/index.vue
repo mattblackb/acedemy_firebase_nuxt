2481b6fda4b95caf6f89f6cf37aa9622ea1acfc6
mattblackb <87385656+mattblackb@users.noreply.github.com>
2023-08-06 14:13:20 +0100

    Update

--- pages/index.vue
+++ pages/index.vue
@@ -2,7 +2,7 @@
   <main>
     <v-container>
       <h1>Site is currently undergoing maintenance</h1>
-      <h2>Please see Patreon for updates</h2>
+      <h2>Please see Patreon for updates..</h2>
     </v-container>
   </main>
 </template>
