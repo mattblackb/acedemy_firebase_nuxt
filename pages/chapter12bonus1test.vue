9e5719fa9b606b249fb5e475d0faafd00383df89
dsp3000-games <106341358+dsp3000-games@users.noreply.github.com>
2023-10-04 16:55:43 +0100

    A few more Ch12 updates

--- pages/chapter12bonus1test.vue
+++ pages/chapter12bonus1test.vue
@@ -7,7 +7,7 @@
           <h1></h1>
           <div class="container">
             <iframe
-              src="../chapter11/game/start11.html"
+              src="../chapter12/game/street240_locked.html"
               width="100%"
               height="100px"
               style="border: 1px solid #eee; background: white"
