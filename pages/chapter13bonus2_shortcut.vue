36722432891fa16dfd3749a18df249e145137367
dsp3000-games <106341358+dsp3000-games@users.noreply.github.com>
2024-01-07 19:17:27 +0000

    Updated all shortcut files to include bonus modal callout code

--- pages/chapter13bonus2_shortcut.vue
+++ pages/chapter13bonus2_shortcut.vue
@@ -7,7 +7,7 @@
           <h1></h1>
           <div class="container">
             <iframe
-              src="../chapter7/game/lunch90_shortcut.html"
+              src="../chapter13/game/didi50_shortcut.html"
               width="100%"
               height="100px"
               style="border: 1px solid #eee; background: white"
