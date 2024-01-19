9d4afe410c0d60688b6e982fbd7ee2c37fc43079
mattblackb <87385656+mattblackb@users.noreply.github.com>
2024-01-19 13:44:40 +0000

    Revert saved game

--- components/saveGame.vue
+++ components/saveGame.vue
@@ -1,10 +1,6 @@
 <template>
   <v-row class="pa-5">
     <p>{{ currentMessage }}</p>
-    <h1>Save Game</h1>
-    <h4>You can add a description to this save if you wish</h4>
-    <v-text-field v-model="description" label="Description"></v-text-field>
-    <v-btn @click="saveEpisode" color="primary">Save Game</v-btn>
   </v-row>
 </template>
 
@@ -15,18 +11,10 @@ export default {
     return {
       currentBuy: this.$route.name,
       selectedStory: {},
-      currentMessage: '',
+      currentMessage: 'Saving your data...',
       storyId: '',
       cookies: {},
-      description: '',
-      currentPerson: {},
     }
-  },
-  watch: {
-    // cookieJson(newValue) {
-    //      console.log('cookieJson changed to: ', newValue, 'route', this.route)
-    //   this.setSave(newValue)
-    // },
   },
   props: {
     cookieJson: {
@@ -37,24 +25,11 @@ export default {
       type: String,
       required: false,
     },
-    dialogSave: {
-      type: Boolean,
-      required: false,
-    },
-    closeWindow: {
-      type: Function,
-      required: false,
-    },
   },
   methods: {
     ...mapGetters('stories', ['getStories']),
-    setSave(newSessionData) {
-      //   alert('changed your route')
-      //   console.log('Route', this.route)
-      this.saveEpisode(newSessionData)
-    },
 
-    saveEpisode(newSessionData) {
+    saveEpisode() {
       let personData = JSON.parse(JSON.stringify(this.$store.state.person))
       const today = new Date()
       const yyyy = today.getFullYear()
@@ -65,39 +40,28 @@ export default {
       if (mm < 10) mm = '0' + mm
 
       const formattedDate = dd + '/' + mm + '/' + yyyy
+      this.cookies['date'] = formattedDate
 
-      //turn json object to string
-      this.cookies = JSON.stringify(this.cookieJson)
-      console.log('this.cookies', this.cookies)
-      //create an array to stor chapte rname and cookies
-      let chapterArray = {}
-      chapterArray['gameDetails'] = this.cookieJson
-      chapterArray['dateSaved'] = formattedDate
-      chapterArray['chapterSaved'] = this.route
-      chapterArray['description'] = this.description
-      personData.saved_games.push(chapterArray)
+      personData.saved_games.push(this.cookies)
       this.$store.commit('setuser/updatePerson', personData)
       this.$store.commit('SET_PEOPLE', personData)
-      this.currentPerson = personData
-      this.currentMessage = 'This game has been saved.'
-
-      this.closeWindow()
+      this.currentMessage =
+        'This game has been saved. You can see this game in your profile'
     },
   },
   mounted() {
-    console.log('MOUNTED')
+    var decodedData = decodeURIComponent(this.cookieJson)
+    // decodedData= atob(decodedData);
+    var jsonObject = JSON.parse(decodedData)
     if (this.route != '') {
-      var decodedData = decodeURIComponent(this.cookieJson)
-      // decodedData= atob(decodedData);
-      var jsonObject = JSON.parse(decodedData)
-      if (this.route != '') {
-        jsonObject['episode'] = this.route
-        console.log(jsonObject['episode'])
-      }
-
-      this.cookies = jsonObject
-      // this.saveEpisode()
+      jsonObject['episode'] = this.route
+      console.log(jsonObject['episode'])
+    } else {
+      jsonObject['episode'] = this.currentBuy
     }
+
+    this.cookies = jsonObject
+    this.saveEpisode()
   },
 }
 </script>
