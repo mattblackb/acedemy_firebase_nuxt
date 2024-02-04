b187b8b6fd0890be7eb05fe886877f5b8e79d69a
mattblackb <87385656+mattblackb@users.noreply.github.com>
2024-02-04 16:02:10 +0000

    Testing beta access - do not push to master

--- layouts/default.vue
+++ layouts/default.vue
@@ -1,9 +1,24 @@
 <template>
   <v-app light>
     <v-main>
-      {{ BETA_SITE }}
+      <!-- if the current route is signin or signout, then do not display the main page -->
 
-      <nuxt />
+      {{ isBetaUser() }}
+      <div v-if="isSigninOrSignoutRoute()">
+        <nuxt />
+      </div>
+      <div v-else-if="checkEmailForBetaLinks()">
+        <nuxt />
+      </div>
+      <div v-else>
+        <v-container>
+          <v-row>
+            <h2>This site is for beta testers only..</h2>
+            <nuxt-link to="/auth/signin">Sign in</nuxt-link>
+            <nuxt-link to="/auth/signout">Sign out</nuxt-link>
+          </v-row>
+        </v-container>
+      </div>
       <p class="pa-5">
         Become a supporter at :
         <a href="https://www.patreon.com/dsp3000" target="_blank"
@@ -18,8 +33,40 @@
 export default {
   data() {
     return {
-      BETA_SITE: process.env.BETA_SITE,
+      currentUser: null,
     }
+  },
+  methods: {
+    isBetaUser() {
+      return process.env.BETA_SITE !== undefined
+    },
+    checkEmailForBetaLinks() {
+      if (!this.userDetails) return false
+      if (
+        this.userDetails.email === 'matt@plain.black' ||
+        this.userDetails.email === 'dsp3000@gmx.com' ||
+        this.userDetails.email === 'jimacep@gmail.com'
+      ) {
+        return true
+      } else {
+        return false
+      }
+    },
+    isSigninOrSignoutRoute() {
+      return (
+        this.$route.path === '/auth/signin' ||
+        this.$route.path === '/auth/signout'
+      )
+    },
+    // other methods...
+  },
+  computed: {
+    userDetails() {
+      if (this.$store.state.person) {
+        this.currentUser = this.$store.state.person
+        return this.$store.state.person
+      }
+    },
   },
 }
 </script>
