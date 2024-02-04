a5d37868f1c08f0b05f32f79f87747925913f1b4
mattblackb <87385656+mattblackb@users.noreply.github.com>
2024-02-04 15:31:57 +0000

    ENV variables test

--- layouts/default.vue
+++ layouts/default.vue
@@ -1,25 +1,36 @@
 <template>
   <v-app light>
-
     <v-main>
-         
+      {{ BETA_SITE }}
 
-        <nuxt />
-        <p class="pa-5">Become a supporter at : <a href="https://www.patreon.com/dsp3000" target="_blank">https://www.patreon.com/dsp3000</a></p>
+      <nuxt />
+      <p class="pa-5">
+        Become a supporter at :
+        <a href="https://www.patreon.com/dsp3000" target="_blank"
+          >https://www.patreon.com/dsp3000</a
+        >
+      </p>
     </v-main>
-    
   </v-app>
 </template>
 
 <script>
 export default {
-  
+  data() {
+    return {
+      BETA_SITE: process.env.BETA_SITE,
+    }
+  },
 }
 </script>
 
 <style>
 .theme--light.v-application {
-background: rgb(255,203,203)!important;
-background: linear-gradient(177deg, rgba(255,203,203,1) 0%, rgba(255,50,75,1) 100%)!important;
+  background: rgb(255, 203, 203) !important;
+  background: linear-gradient(
+    177deg,
+    rgba(255, 203, 203, 1) 0%,
+    rgba(255, 50, 75, 1) 100%
+  ) !important;
 }
 </style>
