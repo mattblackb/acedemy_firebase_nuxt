2cbd0908bd34913d4f05e96921ac18ea9a4f4f30
dsp3000-games <106341358+dsp3000-games@users.noreply.github.com>
2024-02-07 01:00:03 +0000

    Ch14 updates

---
+++ static/chapter14/game/ch14message5check.js
@@ -0,0 +1,21 @@
+// displays phone message outcome
+
+function outcome()
+
+{
+
+  var val = readVar("ch14amy_message");
+
+	
+
+    if (val > 0)
+
+        document.write("<img src='buttonsetc/endcards/ch14message5.jpg'>"); 
+
+    else
+	
+	    document.write("<img src='buttonsetc/endcards/ch14message5_blank.jpg'>");
+
+}
+
+outcome();
