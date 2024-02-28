2f19f3739b33e21652eaa2fc8fa48bb15ee2804c
dsp3000-games <106341358+dsp3000-games@users.noreply.github.com>
2024-02-28 13:56:43 +0000

    Ch14 finishing touches...

--- pages/chapter15Details.vue
+++ pages/chapter15Details.vue
@@ -8,44 +8,25 @@
             v-if="introchosen.episode == 'introduction' && showSaved"
             :introAchievments="introchosen"
           />
-          <img src="/imgs/38.png" />
-          <showBonusDetails chapterNumber="14" />
+          <img src="/imgs/39.png" />
+          <showBonusDetails chapterNumber="15" />
         </v-col>
         <v-col cols="12" md="6">
-          <h1>The Academy - Chapter Fourteen</h1>
+          <h1>The Academy - Chapter Fifteen</h1>
 
           <h4 class="clickable">
-            <NuxtLink to="/chapter13Details">< Previous Chapter</NuxtLink> :
-			<NuxtLink to="/chapter15Details">Next Chapter ></NuxtLink>
+            <NuxtLink to="/chapter14Details">< Previous Chapter</NuxtLink> :
+			
           </h4>
           <br />
 		  <p>
-            It's Friday at last and it's a brand new day at the Goodhead Academy.
-			<br /><br />
-			After an eventful week and a fun filled Thursday evening, it's time to start to unravel some of the mess that's been made.
-			<br />
-			Miss Goodhead has taken over and is quickly implementing her new regime.
-			<br />
-			Not everybody is happy about it, and a few people are looking to you to help them figure out what's going on...
-			<br /><br />
-			You need to determine who to help and how to help them...
-			<br />
-			It's not all hard work though, helping people does come with some more immediate rewards and revelations...
-			<br /><br />
-            <b>Chapter Fourteen</b> statistics : 2200 pages :  2500 images<br>
-			: 6 bonus scenes : 40 achievements : 15 bonus achievements : 10 other achievements
-            <br /><br />
-            <b>NOTE:</b> You can play Chapter Fourteen without playing any of the previous chapters!
+            <h2>COMING SOON</h2>
           </p>
 
           <hr>
-		  <h3 class="clickable">
-            <NuxtLink to="/chapter14">Skip the previous chapters and play Chapter Fourteen</NuxtLink>
-          </h3>
-		  <hr>
           <h3>Continue the story from a saved game</h3>
 
-          <SavedGames chapter="14" />
+          <SavedGames chapter="15" />
         </v-col>
       </v-row>
     </v-container>
