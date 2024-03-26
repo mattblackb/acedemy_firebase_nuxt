9af04aa1c07bd1c1ea02fa35f92636da173dc8cb
dsp3000-games <106341358+dsp3000-games@users.noreply.github.com>
2024-03-26 22:59:09 +0000

    Ch15 upload
    Same as on Beta Tangerine but will be updated

---
+++ static/chapter15/game/_functions_ch15beta2.js
@@ -0,0 +1,57 @@
+function hideShowButton() {
+  let showcontinue = checkAvailable('15BETA2_BETA')
+  if (showcontinue) {
+    document.getElementById('buybutton').style.display = 'none'
+    document.getElementById('availableButton').style.display = 'block'
+    document.getElementById('buybuttonImage').style.display = 'none'
+    document.getElementById('availableButtonImage').style.display = 'block'
+  } else {
+    document.getElementById('buybutton').style.display = 'block'
+    document.getElementById('availableButton').style.display = 'none'
+    document.getElementById('buybuttonImage').style.display = 'block'
+    document.getElementById('availableButtonImage').style.display = 'none'
+  }
+  function moduleAvailable(state) {
+    if (state) {
+      document.getElementById('buybutton').style.display = 'none'
+      document.getElementById('availableButton').style.display = 'block'
+      document.getElementById('buybuttonImage').style.display = 'none'
+      document.getElementById('availableButtonimage').style.display = 'block'
+    }
+  }
+}
+function hideShowButton2() {
+  let showcontinue = checkAvailable('15BETA2_BETA')
+  if (showcontinue) {
+    document.getElementById('buybutton').style.display = 'none'
+    document.getElementById('availableButton').style.display = 'block'
+    document.getElementById('buybuttonImage').style.display = 'none'
+    document.getElementById('availableButtonImage').style.display = 'block'
+  } else {
+    document.getElementById('buybutton').style.display = 'block'
+    document.getElementById('availableButton').style.display = 'none'
+    document.getElementById('buybuttonImage').style.display = 'block'
+    document.getElementById('availableButtonImage').style.display = 'none'
+  }
+  function moduleAvailable(state) {
+    if (state) {
+      document.getElementById('buybutton').style.display = 'none'
+      document.getElementById('availableButton').style.display = 'block'
+      document.getElementById('buybuttonImage').style.display = 'none'
+      document.getElementById('availableButtonimage').style.display = 'block'
+    }
+  }
+}
+
+function checkAvailable(method) {
+  let returnedState = parent.c_1.checkAvailable(method)
+  return returnedState
+}
+
+//Send a message to the parent window to show the dialog box
+function emitCheck(method) {
+  console.log('method', method)
+  parent.c_1.greet(method)
+}
+
+const myTimeout = setInterval(hideShowButton, 1000)
