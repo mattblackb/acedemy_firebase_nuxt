566ee9a03b2e4581cc3e69101865064ec750ad03
dsp3000-games <106341358+dsp3000-games@users.noreply.github.com>
2024-03-26 18:50:15 +0000

    Incomplete Ch15 upload
    Setting at trap for the pirates

---
+++ static/chapter15/game/ch14possess1check.js
@@ -0,0 +1,21 @@
+// displays possession outcome
+
+function outcome()
+
+{
+
+  var val = readVar("ch14bridgette_key");
+
+	
+
+    if (val > 0)
+
+        document.write("<img src='buttonsetc/endcards/ch14possession1.jpg'>"); 
+
+    else
+	
+	    document.write("<img src='buttonsetc/endcards/ch14possession1_blank.jpg'>");
+
+}
+
+outcome();
