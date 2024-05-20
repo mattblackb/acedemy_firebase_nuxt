ac3426381dd7bb156cea7b90f9a2ecfd0cfca01b
dsp3000-games <106341358+dsp3000-games@users.noreply.github.com>
2024-05-20 16:48:38 +0100

    Updates to all chapters : Some more to Ch15

--- static/academy_intro/game/_functions_introbonus.js
+++ static/academy_intro/game/_functions_introbonus.js
@@ -1,5 +1,5 @@
 function hideShowButton() {
-  let showcontinue = checkAvailable('jVPB0ws7x2EDgI8Aq75Z')
+  let showcontinue = checkAvailable('jVPB0ws7x2EDgI8Aq75K')
   if (showcontinue) {
     document.getElementById('buybutton').style.display = 'none'
     document.getElementById('availableButton').style.display = 'block'
@@ -21,7 +21,7 @@ function hideShowButton() {
   }
 }
 function hideShowButton2() {
-  let showcontinue = checkAvailable('jVPB0ws7x2EDgI8Aq75Z')
+  let showcontinue = checkAvailable('jVPB0ws7x2EDgI8Aq75K')
   if (showcontinue) {
     document.getElementById('buybutton').style.display = 'none'
     document.getElementById('availableButton').style.display = 'block'
