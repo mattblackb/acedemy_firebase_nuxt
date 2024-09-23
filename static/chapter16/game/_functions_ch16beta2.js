6b9932f1b1071b59d734c4cc873574b2962faf59
dsp3000-games <106341358+dsp3000-games@users.noreply.github.com>
2024-09-23 17:21:07 +0100

    Updated Beta tracking modules on Beta 2.0

--- static/chapter16/game/_functions_ch16beta2.js
+++ static/chapter16/game/_functions_ch16beta2.js
@@ -1,5 +1,5 @@
 function hideShowButton() {
-  let showcontinue = checkAvailable('16BETA2_0924')
+  let showcontinue = checkAvailable('16BETA2_2309KM')
   if (showcontinue) {
     document.getElementById('buybutton').style.display = 'none'
     document.getElementById('availableButton').style.display = 'block'
@@ -21,7 +21,7 @@ function hideShowButton() {
   }
 }
 function hideShowButton2() {
-  let showcontinue = checkAvailable('16BETA2_0924')
+  let showcontinue = checkAvailable('16BETA2_2309KM')
   if (showcontinue) {
     document.getElementById('buybutton').style.display = 'none'
     document.getElementById('availableButton').style.display = 'block'
